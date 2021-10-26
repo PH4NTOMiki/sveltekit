7cfd54ea135c91542af57b6eccda0a168025e284
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2021-10-26 15:04:59 +0200

    .

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,6 @@
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import Nav from '$lib/../components/Nav.svelte';
 </script>
-<svelte:head><title>Blog</title></svelte:head>
 <!--<nav>
 	<a sveltekit:prefetch href="/">Home</a>
 	<a sveltekit:prefetch href="/blog">Blog</a>
