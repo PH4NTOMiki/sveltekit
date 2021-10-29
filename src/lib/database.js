38f294678bc9e0e1771e35e2e4c0d2676fc5d7dc
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2021-10-29 12:42:34 +0200

    database export types

--- src/lib/database.js
+++ src/lib/database.js
@@ -2,7 +2,10 @@ import * as mongo from 'mongodb';
 
 let client = null;
 let db = null;
-
+/**
+ * 
+ * @returns {{client: mongo.MongoClient, db: mongo.Db}}
+ */
 export async function connectToDB() {
   if(!client) {
     client = await mongo.MongoClient.connect(process.env['VITE_MONGODB_URI'] || import.meta.env.VITE_MONGODB_URI);
